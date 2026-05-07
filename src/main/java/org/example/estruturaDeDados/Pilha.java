f9c942bbc0df1cbc62bdc2b942c287971046da95
João Vitor <joao_vqs@hotmail.com>
2026-05-07 12:09:06 -0300

    Add object Fila

---
+++ src/main/java/org/example/estruturaDeDados/Pilha.java
@@ -0,0 +1,59 @@
+package org.example.estruturaDeDados;
+
+public class Pilha {
+    private Node topo;
+
+    public void push(int valor) {
+
+        Node novo = new Node(valor);
+
+        novo.proximo = topo;
+        topo = novo;
+    }
+
+    public int pop() {
+
+        if (topo == null) {
+            throw new RuntimeException("Pilha vazia");
+        }
+
+        int valor = topo.valor;
+
+        topo = topo.proximo;
+
+        return valor;
+    }
+
+    public int peek() {
+
+        if (topo == null) {
+            throw new RuntimeException("Pilha vazia");
+        }
+
+        return topo.valor;
+    }
+
+    public boolean vazia() {
+        return topo == null;
+    }
+
+    public void imprimir() {
+
+        Node atual = topo;
+
+        while (atual != null) {
+            System.out.println(atual.valor);
+            atual = atual.proximo;
+        }
+    }
+
+    class Node {
+
+        int valor;
+        Node proximo;
+
+        public Node(int valor) {
+            this.valor = valor;
+        }
+    }
+}
